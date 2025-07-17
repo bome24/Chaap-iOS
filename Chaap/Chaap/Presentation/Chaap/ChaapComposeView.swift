63958e0234e2f7630655ae3c0d8e16c74bfdd49d
BoMin <bmlee@postech.ac.kr>
2025-07-17 16:47:37 +0900

    feat: Chaap 작성 뷰 생성

---
+++ Chaap/Chaap/Presentation/Chaap/ChaapComposeView.swift
@@ -0,0 +1,18 @@
+//
+//  ChaapComposeView.swift
+//  Chaap
+//
+//  Created by BoMin Lee on 7/17/25.
+//
+
+import SwiftUI
+
+struct ChaapComposeView: View {
+    var body: some View {
+        Text("기록(챱)을 작성하는 뷰입니다.")
+    }
+}
+
+#Preview {
+    ChaapComposeView()
+}
