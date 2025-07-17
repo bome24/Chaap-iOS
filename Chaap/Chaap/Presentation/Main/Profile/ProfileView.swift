dd039f502b0dd2f0e2487e85d9d00715bf5e6e7c
BoMin <bmlee@postech.ac.kr>
2025-07-17 16:48:08 +0900

    chore: 누락 파일 생성 및 폴더 구조 정리

---
+++ Chaap/Chaap/Presentation/Main/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Chaap
+//
+//  Created by BoMin Lee on 7/17/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("프로필 열람 뷰입니다.")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
