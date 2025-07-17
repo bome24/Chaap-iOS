739a425ab71852e189e678977de0cefd3ee2e5f2
BoMin <bmlee@postech.ac.kr>
2025-07-17 16:47:46 +0900

    feat: Chaap 상세 뷰 생성

---
+++ Chaap/Chaap/Presentation/Chaap/ChaapDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ChaapDetailView.swift
+//  Chaap
+//
+//  Created by BoMin Lee on 7/17/25.
+//
+
+import SwiftUI
+
+struct ChaapDetailView: View {
+    var body: some View {
+        Text("기록(챱) 상세 열람 뷰입니다.")
+    }
+}
+
+#Preview {
+    ChaapDetailView()
+}
