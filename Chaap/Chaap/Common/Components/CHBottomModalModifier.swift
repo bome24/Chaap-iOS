9264a376d04a7c618ebaf62f9d48b400bcf935e5
BoMin <bmlee@postech.ac.kr>
2025-07-21 15:05:29 +0900

    lint: indent 수정

--- Chaap/Chaap/Common/Components/CHBottomModalModifier.swift
+++ Chaap/Chaap/Common/Components/CHBottomModalModifier.swift
@@ -9,67 +9,67 @@ import SwiftUI
 
 struct CHBottomModalModifier<SheetContent: View>: ViewModifier {
     @Binding var isPresented: Bool
-        @GestureState private var dragOffset: CGFloat = 0
-
-        let minHeight: CGFloat
-        let maxHeight: CGFloat
-        let content: () -> SheetContent
-
-        func body(content base: Content) -> some View {
-            ZStack {
-                base
-                    .onTapGesture {
-                        withAnimation {
-                            isPresented = false
-                        }
+    @GestureState private var dragOffset: CGFloat = 0
+    
+    let minHeight: CGFloat
+    let maxHeight: CGFloat
+    let content: () -> SheetContent
+    
+    func body(content base: Content) -> some View {
+        ZStack {
+            base
+                .onTapGesture {
+                    withAnimation {
+                        isPresented = false
                     }
-                if isPresented {
-                    sheetView()
-                        .transition(.move(edge: .bottom))
-                        .animation(.easeInOut, value: isPresented)
                 }
+            if isPresented {
+                sheetView()
+                    .transition(.move(edge: .bottom))
+                    .animation(.easeInOut, value: isPresented)
             }
         }
-
-        @ViewBuilder
-        private func sheetView() -> some View {
-            GeometryReader { geometry in
-                let defaultHeight = maxHeight
+    }
+    
+    @ViewBuilder
+    private func sheetView() -> some View {
+        GeometryReader { geometry in
+            let defaultHeight = maxHeight
+            VStack {
+                Spacer()
                 VStack {
-                    Spacer()
-                    VStack {
-                        Capsule()
-                            .frame(width: 40, height: 4)
-                            .foregroundColor(.white)
-                            .padding(.top, 12)
-                        self.content()
-                            .padding()
-                    }
-                    .frame(width: geometry.size.width, height: defaultHeight - dragOffset)
-                    .background(
-                        CHBlurView(style: .systemUltraThinMaterialDark)
-                            .clipShape(RoundedRectangle(cornerRadius: 32)))
-                    .overlay(GradientStroke())
-                    .cornerRadius(36)
-                    .shadow(radius: 10)
-                    .offset(y: dragOffset)
-                    .gesture(
-                        DragGesture()
-                            .updating($dragOffset) { value, state, _ in
-                                if value.translation.height > 0 {
-                                    state = value.translation.height
-                                }
-                            }
-                            .onEnded { value in
-                                if value.translation.height > 150 {
-                                    withAnimation {
-                                        isPresented = false
-                                    }
-                                }
-                            }
-                    )
+                    Capsule()
+                        .frame(width: 40, height: 4)
+                        .foregroundColor(.white)
+                        .padding(.top, 12)
+                    self.content()
+                        .padding()
                 }
-                .ignoresSafeArea(.all, edges: .bottom)
+                .frame(width: geometry.size.width, height: defaultHeight - dragOffset)
+                .background(
+                    CHBlurView(style: .systemUltraThinMaterialDark)
+                        .clipShape(RoundedRectangle(cornerRadius: 32)))
+                .overlay(GradientStroke())
+                .cornerRadius(36)
+                .shadow(radius: 10)
+                .offset(y: dragOffset)
+                .gesture(
+                    DragGesture()
+                        .updating($dragOffset) { value, state, _ in
+                            if value.translation.height > 0 {
+                                state = value.translation.height
+                            }
+                        }
+                        .onEnded { value in
+                            if value.translation.height > 150 {
+                                withAnimation {
+                                    isPresented = false
+                                }
+                            }
+                        }
+                )
             }
+            .ignoresSafeArea(.all, edges: .bottom)
         }
+    }
 }
