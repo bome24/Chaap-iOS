664c8f5e3c237cc7f70551480383729d71febaa6
HanHyeBeen <vvv5974033@gmail.com>
2025-07-25 16:06:48 +0900

    ✨ [Feat] Chaap Floating Btn Component 구현
    - TagView와 연결 필요

---
+++ Chaap/Chaap/Common/Components/CHFloatingBtn.swift
@@ -0,0 +1,70 @@
+//
+//  CHFloatingBtn.swift
+//  Chaap
+//
+//  Created by Enoch on 7/25/25.
+//
+
+import SwiftUI
+
+struct CHFloatingBtn: View {
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            Image(.chaapBtn)
+                .resizable()
+                .frame(width: 33.37298, height: 31.7691)
+                .padding(26)
+                .background(.black.opacity(0.1))
+                .background(
+                    CHBlurView(style: .systemUltraThinMaterialDark)
+                        .clipShape(Circle())
+                )
+                .clipShape(Circle())
+                .overlay(
+                    ZStack {
+                        Circle()
+                            .inset(by: 0.46)
+                            .stroke(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [
+                                        Color.white.opacity(0),
+                                        Color.white.opacity(0.6 * 0.2)
+                                    ]),
+                                    startPoint: .center,
+                                    endPoint: .bottomTrailing
+                                ),
+                                lineWidth: 0.92
+                            )
+                            .stroke(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [
+                                        Color(hex: "#EEEEEE").opacity(0.8 * 0.2),
+                                        Color(hex: "#EEEEEE").opacity(0)
+                                    ]),
+                                    startPoint: .leading,
+                                    endPoint: .trailing
+                                ),
+                                lineWidth: 0.92
+                            )
+                            .stroke(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [
+                                        Color.white.opacity(0.6),
+                                        Color.white.opacity(0.1)
+                                    ]),
+                                    startPoint: .topLeading,
+                                    endPoint: .center
+                                ),
+                                lineWidth: 0.92
+                            )
+                    }
+                )
+        })
+    }
+}
+
+#Preview {
+    CHFloatingBtn()
+}
