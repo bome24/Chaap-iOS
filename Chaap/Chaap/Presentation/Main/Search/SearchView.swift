6d5facd01a5e826151d57c3d2be7a7993dad40d8
HanHyeBeen <vvv5974033@gmail.com>
2025-07-30 15:32:25 +0900

    feat: search

--- Chaap/Chaap/Presentation/Main/Search/SearchView.swift
+++ Chaap/Chaap/Presentation/Main/Search/SearchView.swift
@@ -6,30 +6,91 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SearchView: View {
     @State private var searchText = ""
     
+    @Query(sort: [SortDescriptor(\Chaap.createdAt, order: .reverse)]) private var allChaaps: [Chaap]
+    
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(spacing: 33) {
                 SearchBar(text: $searchText)
-                
-                List {
-//                    ForEach(array.filter{$0.hasPrefix(searchText) || searchText == ""}, id:\.self) {
-//                        searchText in Text(searchText)
-//                    }
-                }
-                .listStyle(PlainListStyle())
-                //화면 터치시 키보드 숨김
-                .onTapGesture {
-                    hideKeyboard()
-                }
+                searchList
             }
             .safeAreaPadding(.horizontal, 16)
             .safeAreaPadding(.top, 9)
         }
+        //화면 터치시 키보드 숨김
+        .onTapGesture {
+            hideKeyboard()
+        }
         .navigationBarBackButtonHidden()
+    }
+    
+    // MARK: - Chaaps filtering
+    var filteredChaaps: [Chaap] {
+        if searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            return allChaaps
+        } else {
+            return allChaaps.filter { (($0.peers.first?.displayName.localizedCaseInsensitiveContains(searchText)) != nil) ||
+                $0.title.localizedCaseInsensitiveContains(searchText) ||
+                $0.memo.localizedCaseInsensitiveContains(searchText) ||
+                $0.place.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+    
+    // MARK: - List
+    var searchList: some View {
+        ScrollView {
+            LazyVStack(spacing: 16) {
+                ForEach(filteredChaaps, id: \.id) { chaap in
+                    HStack(spacing: 16) {
+                        // 상대 프로필 이미지
+                        if let iconName = chaap.peers.first?.iconName {
+                            Image(iconName)
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(Circle())
+                                .shadow(color: .black.opacity(0.1), radius: 3, x: 1.5, y: 1.5)
+                                .shadow(color: .black.opacity(0.05), radius: 1.2, x: 3, y: 3)
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Text(chaap.peers.first?.displayName ?? "이름 없음")
+                                .font(.chSecondaryCaptionMedium)
+                                .foregroundStyle(Color.chLabelBlackSecondary)
+                            
+                            Spacer()
+                            
+                            Text(chaap.title)
+                                .font(.chBodyRegular)
+                                .foregroundStyle(Color.chLabelBlackPrimary)
+                        }
+                        
+                        Spacer()
+                        
+                        VStack(alignment: .trailing) {
+                            Text(chaap.createdAt.formatted(date: .abbreviated, time: .shortened))
+                                .font(.chSecondaryCaptionMedium)
+                                .foregroundStyle(Color.chLabelBlackSecondary)
+                            
+                            Spacer()
+                            
+                            Text(chaap.place)
+                                .font(.chSecondaryCaptionMedium)
+                                .foregroundStyle(Color.chLabelBlackSecondary)
+                        }
+                    }
+                    .frame(height: 25)
+                    .padding(.vertical, 8)
+                }
+            }
+        }
     }
 }
 
